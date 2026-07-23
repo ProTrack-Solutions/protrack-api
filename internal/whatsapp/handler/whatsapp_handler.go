a1cd753c5c4d080df25c554ede7f0766c3b13e39
GabrielFerrarez19 <gabrielferrarez190206@gmail.com>
2026-07-23 17:27:52 -0300

    feat: funções criada para buscar os dados da instancia e desconectar instancia

--- internal/whatsapp/handler/whatsapp_handler.go
+++ internal/whatsapp/handler/whatsapp_handler.go
@@ -40,13 +40,66 @@ func (h *Handler) CreateInstance(c *gin.Context) {
 
 	companyId := companyIdAny.(uuid.UUID)
 
-	
-
-	resp, err := h.service.CreateInstance(c.Request.Context(),companyId)
+	resp, err := h.service.CreateInstance(c.Request.Context(), companyId)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusCreated, gin.H{"qr_code": resp})
+}
+
+// ConnectionState godoc
+// @Summary      Obtém o estado de conexão do WhatsApp
+// @Description  Consulta o status da instância na Evolution API (ex: open, connecting, close) e retorna os dados da conexão.
+// @Tags         whatsapp
+// @Produce      json
+// @Security     BearerAuth
+// @Success      200  {object}  map[string]interface{} 					"Estado atual do WhatsApp obtido com sucesso"
+// @Failure      401  {object}  map[string]interface{}                  "Empresa não autenticada no contexto"
+// @Failure      500  {object}  map[string]interface{}                  "Falha na comunicação com o serviço ou Evolution API"
+// @Router       /whatsapp/instance/connection-state [get]
+func (h *Handler) ConnectonState(c *gin.Context) {
+	companyIdAny, exists := c.Get("company_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "company id is null"})
+		return
+	}
+
+	companyId := companyIdAny.(uuid.UUID)
+
+	state, err := h.service.ConnectonState(c.Request.Context(), companyId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, state)
+}
+
+// DeleteInstance godoc
+// @Summary      Deleta a instância do WhatsApp
+// @Description  Remove/desconecta a instância do WhatsApp associada à empresa do usuário autenticado.
+// @Tags         whatsapp
+// @Produce      json
+// @Security     BearerAuth
+// @Success      204      "Instância deletada com sucesso"
+// @Failure      401      {object}  map[string]interface{}  "Empresa não autenticada no contexto"
+// @Failure      500      {object}  map[string]interface{}  "Falha ao deletar a instância no serviço"
+// @Router       /whatsapp/instance/delete [delete]
+func (h *Handler) Deleteinstance(c *gin.Context) {
+	companyIdAny, exists := c.Get("company_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "company id is null"})
+		return
+	}
+
+	companyId := companyIdAny.(uuid.UUID)
+
+	if err := h.service.DeleteInstance(c.Request.Context(), companyId); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
